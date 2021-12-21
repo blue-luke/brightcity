fd827c75b9fe553b8f7facd9821fdf0356e1cd5e
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2021-12-21 17:33:07 +0000

    Solved loading problem, just needed an end statement

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,5 @@
-# if Rails.env === 'production' 
-#   Rails.application.config.session_store :cookie_store, key: '_brightcity', domain: 'https://brightcity-api.herokuapp.com/'
-# else
-#   Rails.application.config.session_store :cookie_store, key: '_brightcity'
+if Rails.env === 'production' 
+  Rails.application.config.session_store :cookie_store, key: '_brightcity', domain: 'https://brightcity-api.herokuapp.com/'
+else
+  Rails.application.config.session_store :cookie_store, key: '_brightcity'
+end
