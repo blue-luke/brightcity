b6918f3431e90dc6e6a360d146d7be07398c82e4
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2021-12-21 17:40:08 +0000

    User migration and table set up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_20_231943) do
+ActiveRecord::Schema.define(version: 2021_12_21_173817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 2021_12_20_231943) do
     t.string "wrongAnswer1"
     t.string "wrongAnswer2"
     t.string "wrongAnswer3"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
