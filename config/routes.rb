c970f504b8edc22d1407514c7ac4ea20056ca164
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2021-12-22 17:57:59 +0000

    Added items routes to routes.rb. Not sure what items are, not referenced anywhere else in tutorial

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   resources :letters, only: [:index]
-  
-  resources :users, only: [:create, :show, :index]
-  # Alternative for the above is below
-  # post '/users',         to: 'users#create'
-  # get '/users/:user_id', to: 'users#show'
-  # get '/users',          to: 'users#index'
+
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+  get '/logged_in', to: 'sessions#is_logged_in?'
+
+  resources :users, only: [:create, :show, :index] do
+    resources :items, only: [:create, :show, :index, :destroy]
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
