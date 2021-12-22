cabd898ff134ffed55364e2d498244a421123aec
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2021-12-22 17:29:46 +0000

    Added routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   resources :letters, only: [:index]
+  
+  resources :users, only: [:create, :show, :index]
+  # Alternative for the above is below
+  # post '/users',         to: 'users#create'
+  # get '/users/:user_id', to: 'users#show'
+  # get '/users',          to: 'users#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
